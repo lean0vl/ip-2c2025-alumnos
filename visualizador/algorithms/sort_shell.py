a264bf9bce7b41e6320090328a4d9f6e53ccae6a
lean0vl <leaaaaannnn5@gmail.com>
2025-11-30 01:26:53 -0300

    Solucionado, no devolvía la cantidad de swaps realizados

--- visualizador/algorithms/sort_shell.py
+++ visualizador/algorithms/sort_shell.py
@@ -28,13 +28,14 @@ def step():
     global items, n, salto, indice, indice_interno, guardado, fase, conteo_swaps
 
     if fase == "fin":
-        return {"done": True, "swaps": conteo_swaps}
+        print("Sort Shell – swaps totales:", conteo_swaps)
+        return {"done": True}
 
     # fase: nuevo_salto
     if fase == "nuevo_salto":
         if salto == 0:
             fase = "fin"
-            return {"done": True, "swaps": conteo_swaps}
+            return {"done": False}
         indice = salto
         fase = "nuevo_indice"
         return {"a": indice, "b": indice - salto, "swap": False, "done": False}
@@ -57,7 +58,7 @@ def step():
             a = indice_interno
             b = indice_interno - salto
             indice_interno -= salto
-            conteo_swaps += 1 
+            conteo_swaps += 1
             return {"a": a, "b": b, "swap": True, "done": False}
 
         items[indice_interno] = guardado
@@ -65,4 +66,4 @@ def step():
         fase = "nuevo_indice"
         return {"a": indice_interno, "b": indice_interno, "swap": False, "done": False}
 
-    return {"done": True, "swaps": conteo_swaps}
+    return {"done": True}
